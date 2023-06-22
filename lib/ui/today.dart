c96cbaa5296c50c3bead901ca3768d7a662f1653
lauralam <lamruanolaura@gmail.com>
2023-06-22 19:29:42 -0400

    add drawer and listTitle

--- lib/ui/today.dart
+++ lib/ui/today.dart
@@ -1,5 +1,6 @@
 import 'package:block/themes/theme.dart';
 import 'package:block/ui/eventWidget.dart';
+import 'package:block/ui/info.dart';
 import 'package:block/ui/procesing_work_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,6 @@ class Today extends StatefulWidget {
 }
 
 class _TodayState extends State<Today> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +21,66 @@ class _TodayState extends State<Today> {
           title: Center(child: Text('TaskMe')),
           actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
         ),
-        body: Stack(
-          children: const [
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Menú'),
+                decoration: BoxDecoration(
+                  color: lightColorScheme.primary,
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Perfil'),
+                onTap: () {
+                  // Acción al pulsar el elemento del menú
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.campaign_outlined),
+                title: Text('Recordatorios'),
+                onTap: () {
+                  // Acción al pulsar el elemento del menú
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.info_outline),
+                title: Text('Info'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Info()),
+                  );
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.palette_outlined),
+                title: Text('Temas'),
+                onTap: () {
+                  // Acción al pulsar el elemento del menú
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.key),
+                title: Text('Clave de acceso'),
+                onTap: () {
+                  // Acción al pulsar el elemento del menú
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.delete_forever_outlined),
+                title: Text('Papelera'),
+                onTap: () {
+                  // Acción al pulsar el elemento del menú
+                },
+              ),
+            ],
+          ),
+        ),
+        body: const Stack(
+          children: [
             Positioned(
               top: 16, // Margen superior de 16 puntos
               left: 0,
@@ -54,19 +112,29 @@ class _TodayWidgetState extends State<TodayWidget> {
       width: 300,
       height: 400,
       decoration: BoxDecoration(
-          color: lightColorScheme.primary,
-          shape: BoxShape.circle,),
+        color: lightColorScheme.primary,
+        shape: BoxShape.circle,
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SizedBox(height: 40,),
-          Center(child: Text('Hoy', style:TextStyle(fontSize: 60, color: lightColorScheme.secondaryContainer, fontFamily: "Garamond") ,)),
-          SizedBox(height: 30,),
+          SizedBox(
+            height: 40,
+          ),
+          Center(
+              child: Text(
+            'Hoy',
+            style: TextStyle(
+                fontSize: 60,
+                color: lightColorScheme.secondaryContainer,
+                fontFamily: 'Garamond'),
+          )),
+          SizedBox(
+            height: 30,
+          ),
           EventWidget(),
         ],
       ),
     );
   }
 }
-
-
